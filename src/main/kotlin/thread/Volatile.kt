315f7ce9803e9c28f79a589ebad994aa3db004dd
meihu <meihu1986@163.com>
2017-11-27 13:01:40 +0800

    完善线程安全

--- src/main/kotlin/thread/Volatile.kt
+++ src/main/kotlin/thread/Volatile.kt
@@ -1,28 +1,45 @@
 package thread
 
+import java.util.*
+import kotlin.jvm.Volatile
+
 /**
  * Volatile关键字保证可见性
  */
-private class Print : Runnable {
-    //@Volatile
-    var i = 1
-    //@Volatile
-    var ok = false
+private class Print {
 
-    override fun run() {
-        while (!ok) {
+    @Volatile
+    var running = true
+
+    var count = 0
+
+    fun foo() {
+        while (running) {
             Thread.yield()
         }
-        if (i == 1) println(i)
+        if (count == 0) println("Thread finished, count is $count")
+    }
+
+    fun bar() {
+        count = Random().nextInt(5) + 1
+        running = false
+    }
+
+}
+
+private fun test1() {
+    while (true) {
+        val print = Print()
+        val t1 = Thread({ print.foo() })
+        val t2 = Thread({ print.bar() })
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
     }
 }
 
-fun main(args: Array<String>) {
-    for (i in 1..10000) {
-        val print = Print()
-        Thread(print).start()
-        print.i = 10
-        print.ok = true
-    }
 
+fun main(args: Array<String>) {
+    test1()
 }
